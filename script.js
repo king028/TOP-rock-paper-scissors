a87987dadceda487c3206d54c7e266b743b812f7
mario <mario28sanchez28@gmail.com>
2022-09-15 19:51:36 -0400

    add functions and variables
    created a div in which I put 3 buttons representing either rock, paper or
    scissors. each of the buttons has a event listener allowing me to pass the
    information of the choice and return a result of the winner.

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+const div = document.createElement('div'); // created a div to the buttons inside it
+div.id = 'container';
+document.body.appendChild(div);
+
+const rock = document.createElement('button');
+rock.addEventListener('click', () => {
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    singleRound(playerSelection, computerSelection);
+    scoreTracker(playerScore, computerScore, draw);
+    pickWinner(playerScore, computerScore);
+});
+rock.classList.add('rockBtn');
+rock.textContent = 'Rock';
+div.appendChild(rock);
+
+const paper = document.createElement('button');
+paper.addEventListener('click',() => {
+    const playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    singleRound(playerSelection, computerSelection);
+    scoreTracker(playerScore, computerScore, draw);
+    pickWinner(playerScore, computerScore);
+});
+paper.classList.add('paperBtn');
+paper.textContent = 'Paper';
+div.appendChild(paper);
+
+const scissors = document.createElement('button');
+scissors.addEventListener('click', () => {
+    const playerSelection = 'scissors';
+    const computerSelection = getComputerChoice();
+    singleRound(playerSelection, computerSelection);
+    scoreTracker(playerScore, computerScore, draw);
+    pickWinner(playerScore, computerScore);
+});
+scissors.classList.add('scissorsBtn');
+scissors.textContent = 'Scissors';
+div.appendChild(scissors);
+
+
+const displayDiv = document.createElement('div');
+displayDiv.id = 'displayDiv';
+document.body.appendChild(displayDiv);
+
+const p = document.createElement('p');
+
+const score = document.createElement('div');
+displayDiv.appendChild(score);
+
+const playerSc = document.createElement('span');
+const computerSc = document.createElement('span');
+const DrawR = document.createElement('span');
+
+const h2 = document.createElement('h2');
+
+
+
+
 // adding a function the will randomly return one out of three choices
 function getComputerChoice () {
     let choices = ['rock', 'paper', 'scissors'];
@@ -6,79 +65,62 @@ function getComputerChoice () {
     return random;
 }
 
-
 let playerScore = 0;
-//let oldPlayerScore = playerScore;
-
 let computerScore = 0;
-//let oldComputerScore = computerScore;
-
 let draw = 0;
-//let oldDraw = draw;
 
 //this function will decide the winner between player and computer
 function singleRound (playerSelection, computerSelection) {    
 
+
     if (playerSelection === computerSelection){
-        return drawMatch;
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return playerWin;
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return computerWin;
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return playerWin;
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-       return computerWin;
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return playerWin;
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return computerWin;
+        p.innerText = 'It\'s a draw';
+        draw += 1;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        p.innerText = 'player won rock beats scissors';
+        playerScore += 1;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        p.innerText = 'Computer won paper beats rock';
+        computerScore += 1;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        p.innerText = 'player won paper beats rock';
+        playerScore += 1;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        p.innerText = 'Computer won scissors beats paper';
+        computerScore += 1;
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        p.innerText = 'player won scissors beats paper';
+        playerScore += 1;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        p.innerText = 'Computer won rock beats scissors';
+        computerScore += 1;
     };
-
-    
+    displayDiv.appendChild(p);
 }
 
-
-let playerWin = 'player won';
-let computerWin = 'Computer won';
-let drawMatch = 'It\'s a draw';
-
-
-
-
-//Here we will loop over five rounds of the games and keep the scores
-function game() {
-
-
-    for(let i = 0; i < 5; i++){
-
-        const playerSelection = prompt('Enter your choice rock paper or scissors:').toLowerCase();
-        const computerSelection = getComputerChoice();
-        let winner = singleRound(playerSelection, computerSelection);
-        console.log(winner);
-
-        if(winner == playerWin){
-            playerScore += 1;
-            alert('player wins this round');
-        }else if (winner == computerWin){
-            computerScore += 1;
-            alert('Computer wins this round');
-        }else if(winner == drawMatch){
-            draw += 1;
-            alert('This round is a draw');
-        }
-    };
-
-    alert('Game over');
-    
-
-    if(playerScore > computerScore){
-        alert('Player wins game with: ' + playerScore + ' points');
-    } else if (computerScore > playerScore){
-        alert('Computer wins the game with: ' + computerScore + ' points');
-    }else {
-        alert('no winner');
-    }
+const pickWinner = (playerScore, computerScore) => { //decides winner once they hit 5 points and shows the results with a text
+if(playerScore === 5){
+    h2.classList.add('player-won');
+    h2.innerText = `Congratulations, You Won! ${playerScore} to ${computerScore}`;
+    displayDiv.appendChild(h2);
+} else if(computerScore === 5){
+    h2.classList.add('computer-won');
+    h2.innerText = `Too bad you lost ${playerScore} to ${computerScore}`;
+    displayDiv.appendChild(h2);
 }
 
-game();
+}
+
+const scoreTracker = (playerScore, computerScore, draw) => { // keeping track of the scores and showing it in the webpage
+    playerSc.classList.add('pScore');
+    playerSc.innerText = `Player score: ${playerScore}`;
+    score.appendChild(playerSc);
+
+    computerSc.classList.add('cScore');
+    computerSc.innerText = ` Computer score: ${computerScore}`;
+    score.appendChild(computerSc);
+
+    DrawR.classList.add('pScore');
+    DrawR.innerText = ` Draws: ${draw}`;
+    score.appendChild(DrawR);
+}
